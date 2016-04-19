41e3bc57eb6e3c8d6ffb761bb4962029672c40f5
Belyaev Andrey <andryb.smile@gmail.com>
2016-04-19 03:36:06 +0300

    Sprint 4, Story 8, Test: Length of result limit

---
+++ NumberSystemCalculator/src/test/java/ru/spb/eltech/numbersystemcalculator/logic/ResultLenghtLimitTest.java
@@ -0,0 +1,60 @@
+package ru.spb.eltech.numbersystemcalculator.logic;
+
+import org.junit.Assert;
+import org.junit.Test;
+import ru.spb.eltech.numbersystemcalculator.exception.ExceedResultLenghtLimitException;
+
+/**
+ * Created by andry on 19.04.2016.
+ */
+public class ResultLenghtLimitTest {
+
+    private final int RESULT_MAX_LENGTH = 3;
+
+    private final Calculator calculator = new CalculatorImpl(10, 10);
+
+    private final String[][] validPairNumbers = new String[][] {
+            {"0", "1"},
+            {"0", "1000"},
+            {"1", "1"},
+            {"1", "10"},
+            {"1", "99"},
+            {"2", "2"},
+            {"9", "11"},
+    };
+
+    private final String[][] notValidPairNumbers = new String[][] {
+            {"1", "100"},
+            {"1", "1000"},
+            {"10", "10"},
+            {"50", "50"},
+            {"9999", "9999"},
+    };
+
+    @Test
+    public void testValidResult()
+    {
+        for (String[] pair : validPairNumbers)
+        {
+            String result = calculator.calculate(pair[0], pair[1], Operation.MULTIPLICATION);
+            Assert.assertNotNull(result);
+            Assert.assertTrue(result.length() < 3);
+        }
+    }
+
+    @Test
+    public void testNotValidResult()
+    {
+        for (String[] pair : notValidPairNumbers)
+        {
+            try
+            {
+                calculator.calculate(pair[0], pair[1], Operation.MULTIPLICATION);
+            }
+            catch (Exception e)
+            {
+                Assert.assertTrue(e instanceof ExceedResultLenghtLimitException);
+            }
+        }
+    }
+}
